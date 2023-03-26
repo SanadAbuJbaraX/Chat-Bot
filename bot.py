dfba1ff73d0e70824cd0e89945a9cee31668949a
Sanad Abujbara <127672776+SanadAbuJbaraX@users.noreply.github.com>
2023-03-26 21:51:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import openai
 class ChatBot:
     def __init__(self):
         # set the api key
-        openai.api_key  = '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' #XXX this will be empty so put your own api key but in the ready apps it is set up but here it isn't for security reasons
+        openai.api_key  = '' #XXX this will be empty so put your own api key but in the ready apps it is set up but here it isn't for security reasons
     def get_response(self,user_input):
         # Return Bot Response
         return openai.Completion.create(
